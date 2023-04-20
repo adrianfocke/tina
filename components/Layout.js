ccdfd54a9c69b3fd7f54010f06d6d495c14c8fc2
Focke Adrian <adrian.focke@extern.a1.at>
2023-04-20 11:59:43 +0200

    check in stuff

--- components/Layout.js
+++ components/Layout.js
@@ -4,27 +4,25 @@ import { APP_DESCRIPTION, APP_TITLE } from "../constants";
 import styles from "./Layout.module.scss";
 
 export const Layout = (props) => {
-  return (
-    <>
-      <Head>
-        <title>{APP_TITLE}</title>
-        <meta name="description" content={APP_DESCRIPTION} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  return <>
+    <Head>
+      <title>{APP_TITLE}</title>
+      <meta name="description" content={APP_DESCRIPTION} />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <header>
-        <Link href="/">
-          {/* // TODO think about applying classes, remove this one also */}
-          <a className={styles.links}>Home</a>
-        </Link>
-        <Link href="/imprint">
-          <a className={styles.links}>Imprint</a>
-        </Link>
-      </header>
+    <header>
+      <Link href="/" className={styles.links}>
+        {/* // TODO think about applying classes, remove this one also */}
+        Home
+      </Link>
+      <Link href="/imprint" className={styles.links}>
+        Imprint
+      </Link>
+    </header>
 
-      <main>
-        {props.children}
-      </main>
-    </>
-  );
+    <main>
+      {props.children}
+    </main>
+  </>;
 };
