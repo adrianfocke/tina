2791879e60bb7825ecfbd7e360551bc4c3d7dee0
Focke Adrian <adrian.focke@extern.a1.at>
2023-04-22 18:46:53 +0200

    check in stuff

--- blocks/ResponsiveImage.tsx
+++ blocks/ResponsiveImage.tsx
@@ -5,13 +5,15 @@ import { StyleSheet } from "../types/other";
 export type ResponsiveImageProps = {
   height: number;
   heightUnit?: HeightUnit;
-  image: string;
+  image: any;
   objectFit: NonNullable<JSX.IntrinsicElements["img"]["style"]>["objectFit"];
   priority?: boolean;
   width: number;
   widthUnit?: WidthUnit;
   withBoxShadow?: boolean;
 };
+
+const srcAlt = require("../public/uploads/joy.jpeg");
 
 export default ({
   height,
@@ -22,25 +24,29 @@ export default ({
   width,
   widthUnit,
   withBoxShadow,
-}: ResponsiveImageProps): JSX.Element => (
-  <div
-    style={{
-      ...styles.image,
-      height: `${height}${heightUnit ? heightUnit : "px"}`,
-      width: `${width}${widthUnit ? widthUnit : "px"}`,
-    }}
-  >
-    <Image
-      // TODO
-      alt="IMAGE"
-      src={image}
-      objectFit={objectFit}
-      priority
-      fill
-      sizes="100vw"
-    />
-  </div>
-);
+}: ResponsiveImageProps): JSX.Element => {
+  console.log("Image: ", image.default ? image : srcAlt, image)
+  const img = image.default ? image : srcAlt;
+  return (
+    <div
+      style={{
+        ...styles.image,
+        height: `${height}${heightUnit ? heightUnit : "px"}`,
+        width: `${width}${widthUnit ? widthUnit : "px"}`,
+      }}
+    >
+      <Image
+        // TODO
+        alt="IMAGE"
+        src={img}
+        objectFit={objectFit}
+        priority
+        fill
+        sizes="100vw"
+      />
+    </div>
+  )
+};
 
 const styles: StyleSheet = {
   image: {
